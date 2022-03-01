84bf692f58ac1492ad943246ea03bf1842925cac
Manish-Kumar-S <smanishkumar2002@gmail.com>
2022-03-01 22:40:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex-container">
+       <div className="flex-child-signup">
+      <div className="head">Signup</div>
+     <form>
+       <div className="inputf">
+          <input type="text" placeholder="First Name" required></input>
+       </div>
+       <div className="inputf">
+          <input type="text" placeholder="Last Name" required></input>
+       </div>
+       <div className="inputf">
+          <input type="text" placeholder="Contact Number" required></input>
+       </div>
+       <div className="inputf">
+          <input type="email" placeholder="Email" required></input>
+       </div>
+       <div className="inputf">
+          <input type="text" placeholder="College(if you're a student)"></input>
+       </div>
+       <div className="inputf">
+          <input type="text" placeholder="Password" required></input>
+       </div>
+       <div className="inputf">
+          <input type="password" placeholder="Confirm Password" required></input>
+       </div>
+       <div className='signupl'>
+         <a className='links' href='#'>Signup</a>
+       </div>
+       <div className='signinl'>
+         <p>Already have an account?</p>
+         <a className='links' href='#'>Sign in</a>
+       </div>
+       
+
+     </form>
+
+    </div>
+    <div className='flex-child-photo'>
+           
+    </div>
     </div>
   );
 }
